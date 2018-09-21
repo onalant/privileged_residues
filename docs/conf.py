571f926efc0bb71f1ea4d28155c209fb8f394c32
Tarik Onalan <onalant@users.noreply.github.com>
2018-09-20 18:19:12 -0700

    Edit configuration
    Allow api/privileged_residues.rst

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['api/privileged_residues.rst']
+exclude_patterns = ['_build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
