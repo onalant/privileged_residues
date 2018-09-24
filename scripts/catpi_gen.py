ea5299c47973a743d853d546d44f2ac70234fcb7
Tarik Onalan <onalant@users.noreply.github.com>
2018-09-24 09:53:20 -0700

    Rewrite cation-pi processor to bypass Rosetta
    Only pull necessary information from raw text. This is probably a very
    fragile method, but it increases performance significantly.

--- scripts/catpi_gen.py
+++ scripts/catpi_gen.py
@@ -2,6 +2,7 @@ import multiprocessing
 import numpy as np
 import pyrosetta
 import re
+import sys
 
 from itertools import permutations
 
@@ -11,7 +12,7 @@ from rif.hash import XformHash_bt24_BCC6_X3f
 
 # NOTE(onalant): change below to desired ray pair provider
 from privileged_residues.chemical import catpi
-from privileged_residues.geometry import coords_to_transform
+from privileged_residues.geometry import coords_to_transform, create_ray
 from privileged_residues.util import models_from_pdb, numpy_to_rif
 
 dt = [("hash", "u8"), ("residue", "S3"), ("transform", "u8")]
@@ -36,18 +37,50 @@ raygrid = RayToRay4dHash(ori_resl, LEVER, bound=BOUND)
 target_pair = (2, 5)
 assert(2 == len(target_pair))
 
+# WARN(onalant): What follow are fast-and-loose methods to process PDBs into
+#                records. Beware of changing.
+def atom_records_from_pdb(f):
+    records = []
+    for line in open(f):
+        if (not line.startswith("ENDMDL")):
+            if (line.startswith("ATOM")):
+                sys.stdout.flush()
+                records.append(line.strip().split())
+        else:
+            yield records
+            records = []
+
+    if (any(records)):
+        return records
+
+def records_to_coords(records):
+    return np.array(list(map(lambda r: r[6:9], records)), dtype=np.float)
+
+def fxnl_grp(records):
+    return list(filter(lambda r: r[2] in catpi.rsd_to_fxnl_grp[records[0][3]].atoms, records))
+
+def to_raypair(xyzs):
+    ray1 = create_ray(xyzs[1], xyzs[0])
+    ray2 = create_ray(xyzs[2], xyzs[0])
+
+    return (ray1, ray2)
+
 def write_records_from_structure(pipe, posefile):
-    print("[WORKER-%s]" % (re.match(".*(\d+)", multiprocessing.current_process().name).group(1)), posefile)
-    for pose in models_from_pdb(posefile):
+    prefix = "[WORKER-%s]" % (re.match(".*(\d+)", multiprocessing.current_process().name).group(1))
+    print(prefix, posefile)
+    for rs in atom_records_from_pdb(posefile):
         for (a, b) in permutations(target_pair):
-            raypair = catpi.rays_from_residue(pose.residue(a))
-            hashed_rays = raygrid.get_keys(*(numpy_to_rif(r) for r in raypair)).item()
+            rA = list(filter(lambda r: r[5] == str(a), rs))
+            rB = list(filter(lambda r: r[5] == str(b), rs))
 
-            fxA = catpi.rsd_to_fxnl_grp[pose.residue(a).name3()]
-            fxB = catpi.rsd_to_fxnl_grp[pose.residue(b).name3()]
+            xyzA = records_to_coords(fxnl_grp(rA))
+            xyzB = records_to_coords(fxnl_grp(rB))
 
-            xyzA = np.array([pose.residue(a).xyz(atom) for atom in fxA.atoms])
-            xyzB = np.array([pose.residue(b).xyz(atom) for atom in fxB.atoms])
+            rpair = to_raypair(xyzA)
+
+            hashed_rays = raygrid.get_keys(*(numpy_to_rif(r) for r in rpair)).item()
+
+            fxBName = catpi.rsd_to_fxnl_grp[rB[0][3]].grp
 
             xfA = coords_to_transform(xyzA)
             xfB = coords_to_transform(xyzB)
@@ -55,7 +88,7 @@ def write_records_from_structure(pipe, posefile):
 
             transform = lattice.get_key(xfrel.astype("f4").flatten().view(X3)).item()
 
-            record = np.array([(hashed_rays, fxB.grp, transform)], dtype=dt)
+            record = np.array([(hashed_rays, fxBName, transform)], dtype=dt)
 
             pipe.put(record)
 
